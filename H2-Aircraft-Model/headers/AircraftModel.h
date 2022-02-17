285fb26778f6a64fec7c04d4e8c670590747d238
Calebsakhtar <calebsakhtar@gmail.com>
2022-02-17 21:11:41 +0000

    Allow the ISA function to compute viscosity

--- H2-Aircraft-Model/headers/AircraftModel.h
+++ H2-Aircraft-Model/headers/AircraftModel.h
@@ -7,6 +7,13 @@
 
 namespace AircraftModel {
 
+	// ISA Returns the International Standard Atmosphere(ISA) values for ambient
+	// temperature(op_T) in degrees kelvin, speed of sound(op_a) in m / s, pressure
+	// (op_P)in kPa, density(op_rho) in Kg / m ^ 3, and dynamic viscosity (op_visc) 
+	// in Kg/m/s at a given input height in kilometers(ip_h).
+	void ISA(const double& ip_h, double& op_T, double& op_a, double& op_P, double& op_rho,
+		double& op_visc);
+	
 	// Implements equation (15-20) from:
 	// https://www.sciencedirect.com/topics/engineering/friction-drag-coefficient
 	// Which is the equation for a fully turbulent boundary layer over a flat plate
@@ -15,12 +22,6 @@ namespace AircraftModel {
 	// Re is the reynolds number of the free-stream (needs checking)
 	// M is the Mach number of the free-stream flow
 	double compute_turb_frict_coeff(const double& Re, const double& M);
-
-	// ISA Returns the International Standard Atmosphere(ISA) values for ambient
-	// temperature(op_T) in degrees kelvin, speed of sound(op_a) in m / s, pressure
-	// (op_P)in kPa and density(op_rho) in Kg / m ^ 3 at a given input array height in
-	// kilometers(ip_h).
-	void ISA(const double& ip_h, double& op_T, double& op_a, double& op_P, double& op_rho);
 
 }
 
--- H2-Aircraft-Model/src/AircraftModel.cpp
+++ H2-Aircraft-Model/src/AircraftModel.cpp
@@ -3,40 +3,27 @@
 
 namespace AircraftModel {
 
-	double compute_turb_frict_coeff(const double& Re, const double& M) {
-		// Implements equation (15-20) from:
-		// https://www.sciencedirect.com/topics/engineering/friction-drag-coefficient
-		// Which is the equation for a fully turbulent boundary layer over a flat plate
-		// for air with compressibility accounted for
-		// 
-		// Re is the reynolds number of the free-stream (needs checking)
-		// M is the Mach number of the free-stream flow
-
-		double Cf_turb = 0.455;
-
-		Cf_turb = Cf_turb / pow(log10(Re), 2.58);
-		Cf_turb = Cf_turb / pow(1 + 0.144 * M * M, 0.65);
-
-		return Cf_turb;
-	}
-
-	void ISA(const double& ip_h, double& op_T, double& op_a, double& op_P, double& op_rho) {
+	void ISA(const double& ip_h, double& op_T, double& op_a, double& op_P, double& op_rho,
+		double& op_visc) {
 		// ISA Returns the International Standard Atmosphere(ISA) values for ambient
 		// temperature(op_T) in degrees kelvin, speed of sound(op_a) in m / s, pressure
-		// (op_P)in kPa and density(op_rho) in Kg / m ^ 3 at a given input array height in
-		// kilometers(ip_h).
+		// (op_P)in kPa, density(op_rho) in Kg / m ^ 3, and dynamic viscosity (op_visc) 
+		// in Kg/m/s at a given input height in kilometers(ip_h).
 	
-		// Initialize the relevant quantities at sea level
+		// Initialize the relevant quantities at sea level. Viscosity data from:
+		// http://www.aerodynamics4students.com/properties-of-the-atmosphere/sea-level-conditions.php
 		const double T_sl = 288.15;
 		const double a_sl = 340.3;
 		const double P_sl = 101.325;
 		const double rho_sl = 1.225;
+		const double visc_sl = 1.789e-5;
 
 		// Set the output quantities to equal to 0 (as initialization)
 		op_T = 0;
 		op_a = 0;
 		op_P = 0;
 		op_rho = 0;
+		op_visc = 0;
 
 		if (ip_h <= 0) {
 			// Set the output quantities to equal to the sea level amount if below sea level
@@ -44,6 +31,7 @@ namespace AircraftModel {
 			op_a = a_sl;
 			op_P = P_sl;
 			op_rho = rho_sl;
+			op_visc = visc_sl;
 		}
 		else {
 
@@ -71,10 +59,33 @@ namespace AircraftModel {
 				op_P = op_P * factor;
 				op_rho = op_rho * factor;
 			}
+
+			// Sutherland's Law (from https://www.cfd-online.com/Wiki/Sutherland%27s_law)
+			const double C1 = 1.458e-6;
+			const double S = 110.4;
+
+			op_visc = C1 * pow(op_T, 1.5) / (op_T + S);
 		}
 	}
 
+	double compute_turb_frict_coeff(const double& Re, const double& M) {
+		// Implements equation (15-20) from:
+		// https://www.sciencedirect.com/topics/engineering/friction-drag-coefficient
+		// Which is the equation for a fully turbulent boundary layer over a flat plate
+		// for air with compressibility accounted for
+		// 
+		// Re is the reynolds number of the free-stream (needs checking)
+		// M is the Mach number of the free-stream flow
+
+		double Cf_turb = 0.455;
+
+		Cf_turb = Cf_turb / pow(log10(Re), 2.58);
+		Cf_turb = Cf_turb / pow(1 + 0.144 * M * M, 0.65);
+
+		return Cf_turb;
+	}
+
 	//double compute_delta_CD(const double& current_t, const double& current_length,
-	//	const double& M, const double& cruise_alt) {
+	//	const double& M, const double& cruise_h) {
 	//}
 }
